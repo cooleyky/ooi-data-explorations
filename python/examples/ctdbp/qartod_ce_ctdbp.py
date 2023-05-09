e9fb92d73700fcfa21d09a03d6a28291042d56ed
Kylene Cooley <kylene.cooley@whoi.edu>
2023-05-09 17:02:53 -0400

    Edit scripts to ID dates of recovered data used

--- python/examples/ctdbp/qartod_ce_ctdbp.py
+++ python/examples/ctdbp/qartod_ce_ctdbp.py
@@ -2,10 +2,39 @@
 # -*- coding: utf-8 -*-
 """
 @author Kylene Cooley
-@brief Calls single instance of data request and processing to obtain gross range and 
-    climatology lookup tables for one node of Coastal Endurance Array.
+@brief Calls single instance of data request and processing to obtain the sea water practical 
+    salinity climatology lookup table for one node of Coastal Endurance Array.
 """
-import ooi_data_explorations.qartod.endurance.qartod_ce_ctdbp as qartod_ce
+from ooi_data_explorations.qartod.endurance.qartod_ce_ctdbp import generate_qartod, ANNO_HEADER, CLM_HEADER
+import os
 
+# outline of qartod_ce_ctdbp.main() below minus the gross range table, and set data stream variables 
+#   that are usually set with ooi_data_explorations.common.inputs()
 
-qartod_ce.main(['-s', 'CE01ISSM', '-n', 'SBD17', '-sn', '06-CTDBPC000', '-co', '2021-01-01T00:00:00'])
+# setup the input arguments
+site = 'CE01ISSM'
+node = 'SBD17'
+sensor = '06-CTDBPC000'
+cut_off = '2021-01-01T00:00:00'
+
+# create the QARTOD gross range and climatology lookup values and tables
+annotations, _, clm_lookup, clm_table = generate_qartod(site, node, sensor, cut_off)
+
+# save the downloaded annotations and qartod lookups and tables
+out_path = os.path.join(os.path.expanduser('~'), 'ooidata/qartod/ctdbp')
+out_path = os.path.abspath(out_path)
+if not os.path.exists(out_path):
+    os.makedirs(out_path)
+
+# save the annotations to a csv file for further processing
+anno_csv = '-'.join([site, node, sensor]) + '.quality_annotations.csv'
+annotations.to_csv(os.path.join(out_path, anno_csv), index=False, columns=ANNO_HEADER)
+
+# save the climatology values and table to a csv for further processing
+clm_csv = '-'.join([site, node, sensor]) + '.climatology.csv'
+clm_lookup.to_csv(os.path.join(out_path, clm_csv), index=False, columns=CLM_HEADER)
+parameters = ['sea_water_temperature', 'practical_salinity']
+for i in range(len(parameters)):
+    tbl = '-'.join([site, node, sensor, parameters[i]]) + '.csv'
+    with open(os.path.join(out_path, tbl), 'w') as clm:
+        clm.write(clm_table[i])
